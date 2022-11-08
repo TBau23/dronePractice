ab39db5b8281cafe055885756ed53231e38c1906
Tom Bauer <tbauer1898@gmail.com>
2022-11-08 17:41:58 -0600

    basic setup and keyboard control

---
+++ keyboardControl.py
@@ -0,0 +1,38 @@
+from djitellopy import Tello
+import keyPressModule as kp
+from time import sleep
+
+kp.init()
+tello = Tello()
+tello.connect()
+print(tello.get_battery()) 
+
+def getKeyboardInput():
+    lr, fb, up, yv = 0, 0 , 0 , 0
+    speed = 50
+
+    if kp.getKey("LEFT"): lr = -speed
+    elif kp.getKey("RIGHT"): lr = speed
+
+    if kp.getKey("UP"): fb = speed
+    elif kp.getKey("DOWN"): fb = -speed
+
+    if kp.getKey("q"): yv = -speed
+    elif kp.getKey("e"): yv = speed
+
+    if kp.getKey("z"): up = speed
+    elif kp.getKey("x"): up = -speed
+
+    if kp.getKey("t"): tello.takeoff()
+    if kp.getKey("y"): tello.land()
+
+    return [lr, fb, up, yv]
+
+while True:
+    vals = getKeyboardInput()
+    tello.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    sleep(0.05)
+
+
+
+
